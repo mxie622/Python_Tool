25eb9d690057f70285c9c6865689bd0c7e143b93
mikexie <mxie622@gmail.com>
2019-03-20 16:41:40 +1300

    5 methods
    Modified from :
    
    https://towardsdatascience.com/5-ways-to-detect-outliers-that-every-data-scientist-should-know-python-code-70a54335a623

--- Anomaly_Detection.py
+++ Anomaly_Detection.py
@@ -1,10 +1,11 @@
+# Method 1: standard deviation
 import numpy as np
 import matplotlib.pyplot as plt
 np.random.seed(1)
 anomalies = []
 ind = [] # Anomalies position
 # multiply and add by random numbers to get some real values
-random_data = np.random.randn(5000)  * 20 + 20
+random_data = np.random.randn(500)  * 20 + 20
 
 # Function to Detection Outlier on one-dimentional datasets.
 def find_anomalies(random_data):
@@ -27,3 +28,119 @@ def find_anomalies(random_data):
 
 a = (find_anomalies(random_data))
 print(a[0])
+
+# Method 2: boxplot
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.boxplot(data=random_data)
+plt.show()
+
+
+
+# Method 3: DBSCAN clustering
+from matplotlib import pyplot as plt
+# %matplotlib inline
+from sklearn.cluster import DBSCAN
+import numpy as np
+import pandas as pd
+
+np.random.seed(1)
+random_data = np.random.randn(500, 2) * 20 + 20
+df = pd.DataFrame(random_data)
+
+outlier = DBSCAN(min_samples = 2, eps = 3)
+clusters = outlier.fit_predict(random_data)
+
+list(clusters).count(-1)
+
+# Method 4: Isolation forest; High-dimensional data
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pylab import savefig
+from sklearn.ensemble import IsolationForest
+# Generating data ----
+
+rng = np.random.RandomState(42)
+
+# Generating training data
+X_train = 0.2 * rng.randn(1000, 2)
+X_train = np.r_[X_train + 3, X_train]
+X_train = pd.DataFrame(X_train, columns = ['x1', 'x2'])
+
+# Generating new, 'normal' observation
+X_test = 0.2 * rng.randn(200, 2)
+X_test = np.r_[X_test + 3, X_test]
+X_test = pd.DataFrame(X_test, columns = ['x1', 'x2'])
+
+# Generating outliers
+X_outliers = rng.uniform(low=-1, high=5, size=(50, 2))
+X_outliers = pd.DataFrame(X_outliers, columns = ['x1', 'x2'])
+
+clf = IsolationForest(max_samples=100, random_state=rng)
+clf.fit(X_train)
+
+# predictions
+y_pred_train = clf.predict(X_train)
+y_pred_test = clf.predict(X_test)
+y_pred_outliers = clf.predict(X_outliers)
+
+plt.title("Data")
+
+p1 = plt.scatter(X_train.x1, X_train.x2, c='white',
+                 s=20*4, edgecolor='k')
+p2 = plt.scatter(X_test.x1, X_test.x2, c='green',
+                 s=20*4, edgecolor='k')
+p3 = plt.scatter(X_outliers.x1, X_outliers.x2, c='red',
+                s=20*4, edgecolor='k')
+
+plt.axis('tight')
+plt.xlim((-2, 5))
+plt.ylim((-2, 5))
+plt.legend([p1, p2, p3],
+           ["training observations",
+            "new regular obs.", "new abnormal obs."],
+           loc="lower right")
+
+# saving the figure
+plt.savefig('generated_data.png', dpi=300)
+
+plt.show()
+print("Accuracy:", list(y_pred_test).count(1)/y_pred_test.shape[0])
+# Accuracy: 0.93
+# outliers ----
+print("Accuracy:", list(y_pred_outliers).count(-1)/y_pred_outliers.shape[0])
+# Accuracy: 0.96
+
+# Inspecting the outliers ----
+
+# adding the predicted label
+X_outliers = X_outliers.assign(y = y_pred_outliers)
+
+plt.title("Outlier Inspection")
+
+p1 = plt.scatter(X_train.x1, X_train.x2, c='white',
+                 s=20*4, edgecolor='k')
+p2 = plt.scatter(X_outliers.loc[X_outliers.y == -1, ['x1']],
+                 X_outliers.loc[X_outliers.y == -1, ['x2']],
+                 c='red', s=20*4, edgecolor='k')
+p3 = plt.scatter(X_outliers.loc[X_outliers.y == 1, ['x1']],
+                 X_outliers.loc[X_outliers.y == 1, ['x2']],
+                 c='green', s=20*4, edgecolor='k')
+
+plt.axis('tight')
+plt.xlim((-2, 5))
+plt.ylim((-2, 5))
+plt.legend([p1, p2, p3],
+           ["training observations",
+            "detected outliers",
+            "detected regular obs."],
+           loc="lower right")
+
+# saving the figure
+plt.savefig('outlier_inspection.png', dpi=300)
+
+plt.show()
